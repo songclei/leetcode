f727c5922c7935e9b578d8870b9316c7d7fc46b1
songclei <1500017738@pku.edu.cn>
2019-03-29 17:25:28 +0800

    合并K个排序链表

---
+++ 合并K个排序链表.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    	if (lists.size() == 0)  return NULL;
+		ListNode *ret = lists[0];
+		for (int i = 1; i < lists.size(); ++i) {
+			ret = merge(ret, lists[i]);
+		}
+		return ret;
+    }
+
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+    	if (l1 == NULL)  return l2;
+    	if (l2 == NULL)  return l1;
+    	ListNode* ret = new ListNode(0);
+    	ListNode* p = ret;
+    	while (l1 != NULL && l2 != NULL) {
+    		if (l1->val < l2->val) {
+    			p->next = l1;
+    			l1 = l1->next;
+    		} else {
+    			p->next = l2;
+    			l2 = l2->next;
+    		}
+    		p = p->next;
+    	}
+    	if (l1 != NULL) {
+    		p->next = l1;
+    	}
+    	if (l2 != NULL) {
+    		p->next = l2;
+    	}
+    	return ret->next;
+    }
+};
