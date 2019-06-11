1272dda6f6f04054b67ad5ecaf7e3bb8aac4cc74
songchenlei <songchenlei@bytedance.com>
2019-06-11 23:07:17 +0800

    Length of Last Word

---
+++ lengthOfLastWord.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        int i = n - 1;
+        while (i >= 0 && s[i] == ' ')  i--;
+        int j = i;
+        while (j >= 0 && s[j] != ' ')  j--;
+        return i-j;
+    }
+};
