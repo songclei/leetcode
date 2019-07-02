8860d8428d1635579bfbe475ef71c757b786c5b3
songchenlei <songchenlei@bytedance.com>
2019-07-02 18:35:27 +0800

    Implement Rand10() Using Rand7()

---
+++ inplementRand10UsingRand7.cpp
@@ -0,0 +1,15 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        while (true) {
+            int a = rand7();
+            int b = rand7();
+            if (b <= 4)  return a;
+            if (a <= 4)  return b+3;
+        }
+    }
+};
